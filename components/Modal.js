38974fb0b4b271e4485c4dcf0e7bffdcb5f68c88
Junnnnnnnnnnn <wjsguswls71@naver.com>
2023-08-30 10:48:54 +0900

    feat: result 페이지 수정 및 추가

--- components/Modal.js
+++ components/Modal.js
@@ -1,71 +1,46 @@
-import { useEffect, useRef, useState } from 'react';
-import styles from './modal.module.css';
+import { useEffect, useRef, useState } from "react";
+import styles from "./modal.module.css";
 
-const Modal = ({ setModalOpen}) => {
-  const closeModal = () => {
-      setModalOpen(false);
-      console.log('닫기')
-  };
-
-  //4초뒤 사라짐
-  // const [showImage, setShowImage] = useState(true);
-
-  // useEffect(() => {
-  //   const imageTimer = setTimeout(() => {
-  //     setShowImage(false);
-  //   }, 4000);
-
-  //   return () => clearTimeout(imageTimer);
-  // }, []);
-
-
-
-  const modalRef = useRef(null);
-
-  useEffect(()=>{
-
-    const handler = (event) => {
-      if(modalRef.current && !modalRef.current.contains(event.target)){
-        setModalOpen(false);
-      }
-    };
-
-
-      document.addEventListener('mousedown',handler);
-
-      return () => {
-        document.removeEventListener('mousedown', handler);};
-    }, [setModalOpen]);
-
-  
-
+const Modal = ({ handleOpen, data }) => {
   return (
-
-    <div className={styles['modal-back']}>
-      <div ref={modalRef} className={styles['modal-container']}>
-        <img src='modal_box.png' className={styles['modal-content']}/>
-        <div onClick={closeModal}>
-        <img src='close_btn.png' onClick={closeModal} className={styles['modal_close']}/>
-        </div>
+    <div className={styles["modal-back"]} onClick={handleOpen(false)}>
+      <div
+        className={styles["modal-container"]}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <img src="modal_box.png" className={styles["modal-content"]} />
+        <button onClick={handleOpen(false)}>
+          <img src="close_btn.png" className={styles["modal_close"]} />
+        </button>
         <div>
-        <p className={styles['modal_title']}>시가를 피우는 너구리</p>
-        <div>
-        {/* {showImage && <img src='/tooltip.png' className={styles['modal_tooltip']}/>} */}
-        </div>
-          <img src='/dice.png' className={styles['modal_dice']}/>
-            <div className={styles['modal_img']}>
-              <img src='/spotImg.png'/>
-            </div>
-          <div className={styles['modal_hash']}>
-            <div></div>
-            <div></div>
-            <div></div>
+          <p className={styles["modal_title"]}>{data.name}</p>
+          <div>
+            {/* {showImage && <img src='/tooltip.png' className={styles['modal_tooltip']}/>} */}
           </div>
-          <div className={styles['modal-line']}></div>
-          <div className={styles['modal-result']}>하이용</div>
+          <div className={styles["modal_img"]}>
+            <img src={data.imageUrl} />
+          </div>
+          <div className={styles["modal_hash"]}>
+            {data.tags.map((v, index) => (
+              <p key={`hotspot-modal-${index}`}>{v.tag}</p>
+            ))}
+          </div>
+          <div className={styles["modal-line"]}></div>
+          <div className={styles["modal-result"]}>
+            {data.content.split("\n").map((v, index, array) => (
+              <li
+                key={index}
+                style={{
+                  marginBottom: index !== array.length - 1 ? "30px" : "0",
+                }}
+              >
+                {v}
+              </li>
+            ))}
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
